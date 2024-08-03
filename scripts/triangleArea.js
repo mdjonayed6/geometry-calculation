e7d6f12fe8e6e7213005850bfe8be1ae146e5fb3
mdjonayed6 <jonayed2640@gmail.com>
2024-08-03 16:54:20 +0600

    triangle area calculation functionality is completed

--- scripts/triangleArea.js
+++ scripts/triangleArea.js
@@ -1,3 +1,20 @@
-function checkStatus() {
-    document.
+function triangleAreaCalculation() {
+  const triangleBaseInput = document.getElementById("triangle-base");
+  const triangleBaseText = triangleBaseInput.value;
+  const base = parseFloat(triangleBaseText);
+  console.log(base);
+
+  // get triangle height
+  const triangleHeightInput = document.getElementById("triangle-height");
+  const triangleHeightText = triangleHeightInput.value;
+  const height = parseFloat(triangleHeightText);
+  console.log(height);
+
+  //   area calculation
+  const triangleArea = 0.5 * base * height;
+  console.log("Your are is: ", triangleArea);
+
+  //   display area result
+  const triangleAreaSpan = document.getElementById("triangle-area");
+  triangleAreaSpan.innerText = triangleArea;
 }
